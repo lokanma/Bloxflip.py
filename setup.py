8b4c0cf431af38cc010d3aed4efb82b410dd6883
Alex <114032955+alexwydd@users.noreply.github.com>
2022-09-21 00:03:17 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="Bloxflip.py",
+	version = "1.0.0",
+	author = "alexwyd",
+	description = "An api wrapper for https://bloxflip.com",
+	packages=["bloxflip"]
+)
